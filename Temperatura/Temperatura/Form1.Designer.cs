0377f92f472edf96ffe8b482339ce34698041c4d
madladwolf <48660166+madladwolf@users.noreply.github.com>
2019-06-14 12:14:14 +0100

    Add files via upload

---
+++ Temperatura/Temperatura/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_temp = new System.Windows.Forms.TextBox();
+            this.chk_chover = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_temp
+            // 
+            this.txt_temp.Location = new System.Drawing.Point(13, 13);
+            this.txt_temp.Name = "txt_temp";
+            this.txt_temp.Size = new System.Drawing.Size(100, 20);
+            this.txt_temp.TabIndex = 0;
+            this.txt_temp.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // chk_chover
+            // 
+            this.chk_chover.AutoSize = true;
+            this.chk_chover.Location = new System.Drawing.Point(120, 13);
+            this.chk_chover.Name = "chk_chover";
+            this.chk_chover.Size = new System.Drawing.Size(57, 17);
+            this.chk_chover.TabIndex = 3;
+            this.chk_chover.Text = "Chuva";
+            this.chk_chover.UseVisualStyleBackColor = true;
+            this.chk_chover.CheckedChanged += new System.EventHandler(this.Chk_chover_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Temperatura.Properties.Resources.rain_PNG13459;
+            this.pictureBox2.Location = new System.Drawing.Point(142, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(179, 90);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 167);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 210);
+            this.Controls.Add(this.chk_chover);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_temp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_temp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox chk_chover;
+    }
+}
+
---
+++ Temperatura/Temperatura/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Visible = false;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double temp = double.Parse(txt_temp.Text);
+                if (temp < 3)
+                {
+                    this.BackColor = Color.FromArgb(0, 153, 153);
+                    //Image.FromFile(@'C:\Users\t0114312\source\repos\Temperatira\Temperatira\Imagens\gelo');
+                    pictureBox1.Image = Temperatura.Properties.Resources._23917;
+
+                }
+                else if (temp > 3 && temp < 15)
+                {
+                    pictureBox1.Image = Temperatura.Properties.Resources._49823;
+                    this.BackColor = Color.FromArgb(255, 229, 204);
+                }
+                else if (temp > 15)
+                {
+                    this.BackColor = Color.FromArgb(255, 102, 102);
+                    pictureBox1.Image = Temperatura.Properties.Resources.sol;
+                }
+            }
+            catch
+            {
+                this.BackColor = Color.FromArgb(255, 255, 255);
+            }
+        }
+
+        private void Chk_chover_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_chover.Checked)
+            {
+                pictureBox2.Visible = true;
+                pictureBox2.Image = Temperatura.Properties.Resources.rain_PNG13459;
+            }
+            else
+            {
+                pictureBox2.Visible = false;
+            }
+        }
+    }
+}
